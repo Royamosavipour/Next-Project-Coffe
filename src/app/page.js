e2710d5c8027a68cb62c816e8e2884bff7fa8f37
royamousavipour <roya2002_rm@yahoo.com>
2024-03-16 22:10:09 +0200

    ok

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,6 @@
 import Navbar from "@/components/modules/navbar/Navbar";
 import Banner from "@/components/templates/index/baner/Banner";
+import Lates from "@/components/templates/index/latest/Latest";
 
 
 export default function Home() {
@@ -7,6 +8,7 @@ export default function Home() {
     <>
       <Navbar />
       <Banner/>
+      <Lates/>
     
     
     </>
