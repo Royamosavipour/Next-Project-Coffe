056356b4e1d1318c42e7ccc2adeef000fa255923
royamousavipour <roya2002_rm@yahoo.com>
2024-03-20 09:50:56 +0200

    add pageYoffset

--- src/components/modules/navbar/Navbar.js
+++ src/components/modules/navbar/Navbar.js
@@ -1,5 +1,5 @@
-"use client"
-import React, { useState } from "react";
+"use client";
+import React, { useEffect, useState } from "react";
 import styles from "./Navbar.module.css";
 import Link from "next/link";
 import { IoIosArrowDown } from "react-icons/io";
@@ -7,9 +7,24 @@ import { FaCartShopping, FaRegHeart } from "react-icons/fa6";
 
 function Navbar() {
   const [fixTop, setFixTop] = useState(false);
+
+  useEffect(() => {
+    const fixNavbarToTop = () => {
+      const currentScroll = window.pageYOffset;
+      if (currentScroll > 105) {
+        setFixTop(true);
+      } else {
+        setFixTop(false);
+      }
+    };
+
+    window.addEventListener("scroll", fixNavbarToTop);
+    return () => window.removeEventListener("scroll", fixNavbarToTop);
+  }, []);
+
   return (
     <>
-      <nav className={styles.navbar}>
+      <nav className={fixTop ? styles.navbar_fixed : styles.navbar}>
         <main>
           <div>
             <Link href="/">
