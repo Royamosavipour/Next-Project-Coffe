fc1b65b733c932c8240a7c62f962096a93f2f89c
royamousavipour <roya2002_rm@yahoo.com>
2024-03-23 17:18:55 +0200

    add route modle user

--- models/User.js
+++ models/User.js
@@ -1,13 +1,13 @@
 const mongoose = require("mongoose");
 
-const schema = mongoose.Schema({
+const schema = new mongoose.Schema({
   name: { type: String, require: true },
   email: { type: String, require: false },
   phone: { type: String, require: true },
   password: { type: String, require: false },
   role: { type: String, default: "USER" },
-  refreshToken:String
+  refreshToken: String,
 });
 
-
-export default 
+const model = mongoose.models.User || mongoose.model("User", schema);
+module.exports = model;
--- src/app/api/auth/signup/route.js
+++ src/app/api/auth/signup/route.js
@@ -1,5 +1,41 @@
 import connectToDB from "@/configs/db";
-export async function GET(req) {
+import UserModel from "@/models/User";
+import { generateAccessToken, hashPassword } from "@/utils/auth";
+import { roles } from "@/utils/constants";
+
+export async function POST(req) {
   connectToDB();
-  return Response.json({ message: "success response" }, { status: 201 });
+  const body = await req.json();
+  const { name, phone, email, password } = body;
+
+  const isUserExist = await UserModel.findOne({
+    $or: [{ name }, { email }, { phone }],
+  });
+
+  if (isUserExist) {
+    return Response.json(
+      { message: "The username or email or phone exist already" },
+      { status: 422 }
+    );
+  }
+
+  const hashedPassword = await hashPassword(password);
+  const accessToken = generateAccessToken({ name });
+
+  const users = await UserModel.find({});
+  await UserModel.create({
+    name,
+    email,
+    phone,
+    password: hashedPassword,
+    role: users.length > 0 ? roles.USER : roles.ADMIN,
+  });
+
+  return Response.json(
+    { message: "success response" },
+    {
+      status: 201,
+      headers: { "Set-Cooki": `token=${accessToken};path=/;httpOnly=true` },
+    }
+  );
 }
