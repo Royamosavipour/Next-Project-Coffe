b25c9f7037f63fbfb0afe2bf45a0fb8614768499
royamousavipour <roya2002_rm@yahoo.com>
2024-04-10 11:40:25 +0300

    ok

--- src/components/modules/comment/Comment.js
+++ src/components/modules/comment/Comment.js
@@ -9,14 +9,14 @@ const Comment = ({ username, body, date, score }) => {
         <div className={styles.main_details}>
           <div className={styles.user_info}>
             <strong>{username}</strong>
-            <p>{date}</p>
+            {/* <p>{date}</p> */}
           </div>
           <div className={styles.stars}>
-            {new Array(5 - score).fill(0).map((star) => (
-              <FaRegStar />
+            {new Array(5 - score).fill(0).map((star, index) => (
+              <FaRegStar key={index} />
             ))}
-            {new Array(score).fill(0).map((star) => (
-              <FaStar />
+            {new Array(score).fill(0).map((star, index) => (
+              <FaStar key={index} />
             ))}
           </div>
         </div>
